1c606554788f7f61cb985bcb9a3d8bfc6396cb33
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-04 22:16:58 +0530

    sort numbers in ascending order

---
+++ arrays/sort-numbers.ts
@@ -0,0 +1,16 @@
+
+
+export function sortNumbers(arr:number[]):number[]{
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j <arr.length ; j++) {
+            if(arr[i]>arr[j]){
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+
+}
