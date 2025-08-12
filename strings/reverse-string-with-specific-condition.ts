834f6bbdea42b87044c795ab2ea8bb06692dbd54
umaji bagal <bagalumaji@gmail.com>
2025-08-12 20:23:38 +0530

    reverse-string-with-specific-condition

---
+++ strings/reverse-string-with-specific-condition.ts
@@ -0,0 +1,19 @@
+import { reverseString } from "./reverse-string"
+
+const str = "sharannya bagal pandharpur";
+
+export function reverseStringWithCondition(str:string):string{
+    let result = "";
+    const words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        if(i%2 ===0){
+            result+=reverseString(words[i])+" ";
+        }
+        else{
+            result += words[i].substring(1,words[i].length-1)+" ";
+        }
+    }
+    return result;
+}
+
+console.log(reverseStringWithCondition(str));
